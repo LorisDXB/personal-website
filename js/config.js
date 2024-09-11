3f23fe748d691220b5c9a2f3bc9c1e1104bfcf43
moosasaur <136992967+LorisDXB@users.noreply.github.com>
2024-09-11 11:36:28 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-export const projectDataFile = "../data/projects.json";
-export const plugDataFile = "../data/plugs.json";
-
-export const maxArticles = 4;
--- js/main.js
+++
@@ -1,8 +0,0 @@
-import * as config from "./config.js";
-import {initCatalog} from "./modules/catalog.js";
-import {initPlugs} from "./modules/plugs.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    initCatalog(config.projectDataFile);
-    initPlugs(config.plugDataFile); 
-});
